da8c5f45b856929b2870060a44df7b3d5ede7c52
jazeb <jazeb@codesorbit.com>
2021-06-02 22:39:29 +0500

    fixes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 const app = require('./app');
 const PORT = process.env.PORT;
 
 const URI = process.env.MONGO_URI;
 const mondoconfig = { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
-mongoose.connect(URI, mondoconfig).then(_ => console.log('MongoDB connected'))
+
+mongoose.connect(URI, mondoconfig).then(_ => console.log('MongoDB connected'));
 
 app.listen(PORT, _ => console.log(`server is running on port ${PORT}`));
